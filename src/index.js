ec7be1de8e203910a1c2a6609f47a2b4f3e8630e
dr-mimic <gartflores@gmail.com>
2021-03-03 15:37:43 -0500

    cleaning commented code up

--- src/index.js
+++ src/index.js
@@ -13,101 +13,49 @@ const shuffleArray = array => {
   }
 }
 
-const issues = [
-  'RFGW-32',
-  'RFGW-31'
+let issues = [
+  'issue-A',
+  'issue-B',
+  'issue-C',
+  'issue-D',
+  'issue-E',
+  'issue-F',
+  'issue-G'
 ]
-//september
-// const issues = [
-//   'CCM-141',
-//   'CCM-143',
-//   'CCM-139',
-//   'CCM-138',
-//   'CCM-137',
-//   'DEV-4',
-//   'ME-667',
-//   'ME-12',
-//   'ME-673',
-//   'RFGW-49'
-// ]
-//october
-// const issues = [
-//   'RFGW-77',
-//   'RFGW-78',
-//   'RFGW-79',
-//   'RFGW-76',
-//   'RFGW-66',
-//   'RFGW-80',
-//   'RFGW-85',
-//   'CCM-146',
-//   'CCM-148',
-//   'CCM-149',
-//   'CCM-150',
-//   'CCM-152',
-//   'CCM-153',
-//   'CCM-154',
-//   'CCM-103',
-//   'CCM-155',
-//   'CCM-103',
-//   'RFGW-87',
-//   'RFGW-86',
-//   'RFGW-89',
-//   'RFGW-90',
-//   'RFGW-57',
-//   'RFGW-93',
-//   'RFGW-95',
-//   'RFGW-97',
-//   'RFGW-99',
-//   'RFGW-100',
-//   'RFGW-101',
-//   'RFGW-102',
-//   'RFGW-103',
-//   'RFGW-104',
-//   'RFGW-105',
-//   'RFGW-98',
-//   'RFGW-96',
-//   'RFGW-107',
-//   'RFGW-108',
-//   'RFGW-92',
-//   'RFGW-106',
-//   'RFGW-91',
-//   'DEV-5'
-// ]
 
 let dates = dater.createDates('2020','08','1','17')
 shuffleArray(issues)
 shuffleArray(dates)
-let times = timer.createTimes(issues.length, 11, 1)
 
-/*
-@Todo
-times should be timer.createTimes(dates.length)
-
-times.forEach should grab issueKey at random from issues arr and when
-all issues have been posted, keeps grabbing random issues from array
-*/
+const totalHours = 11
+let times = timer.makeTimes({
+  dates,
+  totalHours,
+  issues
+})
 
 times.forEach( (time,idx) => {
-  axios.post(`${endpoint}/worklogs/`, 
-  {
-    issueKey: issues[idx],
-    timeSpentSeconds: time,
-    startDate: dates[idx],
-    startTime: "06:00:00",
-    authorAccountId: process.env.JIRA_ACT_ID,
-  },
-  { headers: { Authorization: `Bearer ${process.env.TEMPO_API_TOKEN}` }
-  })
-  .then((res) => {
-    console.log(`status:${res.status} data:${res.config.data}`)
-  })
-  .catch( err => {
-    console.log(`err on issue ${issues[idx]}`)
-    console.log({
-      message: err.message,
-      time,
-      startDate: dates[idx],
-      authorAccountId: process.env.JIRA_ACT_ID
-    })
-  })
+  console.log('time created:', time)
+  // axios.post(`${endpoint}/worklogs/`, 
+  // {
+  //   issueKey: issues[idx],
+  //   timeSpentSeconds: time,
+  //   startDate: dates[idx],
+  //   startTime: "06:00:00",
+  //   authorAccountId: process.env.JIRA_ACT_ID,
+  // },
+  // { headers: { Authorization: `Bearer ${process.env.TEMPO_API_TOKEN}` }
+  // })
+  // .then((res) => {
+  //   console.log(`status:${res.status} data:${res.config.data}`)
+  // })
+  // .catch( err => {
+  //   console.log(`err on issue ${issues[idx]}`)
+  //   console.log({
+  //     message: err.message,
+  //     time,
+  //     startDate: dates[idx],
+  //     authorAccountId: process.env.JIRA_ACT_ID
+  //   })
+  // })
 })
--- src/timer/timer.js
+++ src/timer/timer.js
@@ -1,31 +1,63 @@
 const hoursToSeconds = hours => Math.floor(hours * 60 * 60)
 const secondsToHours = seconds => Math.floor(seconds / 3600)
 
-const divvy = (number, parts, min) => {
+const divvy = ({seconds, partsInt}) => {
 
-  const randombit = number - min * parts
+  const randombit = seconds - .5 * partsInt
   let out = []
   let i = 0
   
-  for ( i; i < parts; i++) {
+  for ( i; i < partsInt; i++) {
     out.push( Math.random() )
   }
   
   const mult = Math.floor( randombit / out.reduce(function (a,b) {return a+b;}) )
   
   return out.map( el => {
-    let ret = Math.floor(el * mult + min)
+    let ret = Math.floor(el * mult + .5)
     return ret; 
   })
 
 }
 
-const createTimes = (entries, totalHours, minDay) => {
-  return divvy(hoursToSeconds(totalHours), entries, hoursToSeconds(minDay))
+const makeTimes = ({dates, totalHours, issues}) => {
+  const originalIssues = JSON.parse(JSON.stringify(issues))
+  let clonedIssues = JSON.parse(JSON.stringify(originalIssues))
+  const issuesPerDay = 3
+  let returnArr = []
+  let fullDivvy = divvy({
+    seconds: hoursToSeconds(totalHours),
+    partsInt: dates.length
+  })
+  //console.log('fullDivvy:', fullDivvy)
+  try {
+    dates.map( (date, idx) => {
+
+      clonedIssues = (clonedIssues.length <= 0) ? JSON.parse(JSON.stringify(originalIssues)) : clonedIssues
+      let subIssues = clonedIssues.splice(0,issuesPerDay)
+      let subDivvy = divvy({
+        seconds: fullDivvy[idx],
+        partsInt: subIssues.length
+      })
+      console.log('subIssues:', subIssues)
+      console.log('subDivvy:', subDivvy)
+      subIssues.map( (issueKey, idxx) => {
+        returnArr.push({
+          issueKey,
+          startDate: date,
+          timeSpentSeconds: subDivvy[idxx]
+        })
+      })
+    })
+  } catch(e) {
+    console.error(e)
+  }
+  return returnArr
 }
 
 module.exports = {
-  createTimes,
   hoursToSeconds,
-  secondsToHours
+  secondsToHours,
+  divvy,
+  makeTimes
 }
